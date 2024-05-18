2da835cb39446a24d1277ce3d132f9bd31f19fa2
Rashmi028 <vermarashmi028@gmail.com>
2024-05-18 23:47:04 +0530

    commit

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
@@ -6,6 +5,7 @@ var userClickedPattern = [];
 
 var started = false;
 var level = 0;
+var score = 0;
 
 $(document).keypress(function() {
   if (!started) {
@@ -23,30 +23,32 @@ $(".btn").click(function() {
   playSound(userChosenColour);
   animatePress(userChosenColour);
 
-  checkAnswer(userClickedPattern.length-1);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
 function checkAnswer(currentLevel) {
 
-    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-      if (userClickedPattern.length === gamePattern.length){
-        setTimeout(function () {
-          nextSequence();
-        }, 1000);
-      }
-    } else {
-      playSound("wrong");
-      $("body").addClass("game-over");
-      $("#level-title").text("Game Over, Press Any Key to Restart");
-
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      score++;
+      $("#score").text("Score: " + score);
       setTimeout(function () {
-        $("body").removeClass("game-over");
-      }, 200);
-
-      startOver();
+        nextSequence();
+      }, 1000);
     }
-}
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    $("#score").text("Final Score: " + score);
 
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    startOver();
+  }
+}
 
 function nextSequence() {
   userClickedPattern = [];
@@ -76,4 +78,8 @@ function startOver() {
   level = 0;
   gamePattern = [];
   started = false;
+  score = 0;
+  setTimeout(function () {
+    $("#score").text("Score: " + score);
+  }, 100000); // Reset score display after 2 seconds to ensure the final score is shown first
 }
